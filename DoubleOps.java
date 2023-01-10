14f437f43e81758cba7d48069b22f47deb385371
23NateK <n.kimber22@gmail.com>
2023-01-10 12:37:14 -0600

    magically finished everything very quickly

--- DoubleOps.java
+++ DoubleOps.java
@@ -1,5 +1,17 @@
 class DoubleOps {
     public static void main(String[] args) {
+
+
+        System.out.println(Math.abs(Integer.parseInt(args[0])));
+
+        System.out.println(Math.abs(Double.parseDouble(args[1])));
+
+        System.out.println(Math.pow(Double.parseDouble(args[2]),Double.parseDouble(args[3])));
         
+        System.out.println(Math.sqrt(Double.parseDouble(args[4])));
+
+
+        System.out.println(Math.random());
+
     }
 }
--- Quadratic.java
+++ Quadratic.java
@@ -1,5 +1,15 @@
 public class Quadratic {
     public static void main(String[] args) {
-        
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+
+        System.out.println((-b+Math.sqrt(b*b-4*a*c)/2*a));
+        System.out.println((-b-Math.sqrt(b*b-4*a*c)/2*a));
+
+
+
+
+
     }
 }
